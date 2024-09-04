cafe5940fe3b683881dbca33941090e9b3a16743
shanoukhavi <shanoukh5@gmail.com>
2024-09-04 21:23:05 +0530

    hamming distance ben updated

--- hammingdistance.cpp
+++ hammingdistance.cpp
@@ -6,15 +6,23 @@ int main()
     int x;
     int y;
     cin>>x>>y;  
-   int res=x^y;
-   int another=res>>x;
-    if(y>x){
-        another=res>>x;
-    }
-    else{
-        another=res>>y;
-    }
-    cout<<another;
-
-
+   int ans=x^y;
+int ct=0;
+while(ans){
+    ct+=ans&1;
+    ans>>=1;
+    //ans=ans>>1;
 }
+cout<<ct;
+}
+//    int another=res>>x;
+//     if(y>x){
+//         another=res>>x;
+//     }
+//     else{
+//         another=res>>y;
+//     }
+//     cout<<another;
+
+
+// }
